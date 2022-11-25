6d9d3012a80495b3b2eb56a79e88b41520131381
mintusarker <sarkermintu707@gmail.com>
2022-11-25 13:21:33 +0600

    category API done

--- index.js
+++ index.js
@@ -12,18 +12,25 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dl1tykd.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try {
+        const laptopCategoryCollection = client.db('usedLaptop').collection('itemCategory');
+
+        app.get('/itemCategory', async (req, res) => {
+            const query = {};
+            const result = await laptopCategoryCollection.find(query).toArray();
+            res.send(result)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
 
 
 app.get('/', async (req, res) => {
