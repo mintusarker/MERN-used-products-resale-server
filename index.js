a97d3e191d1488ba8c81422f211ead0017e47592
mintusarker <sarkermintu707@gmail.com>
2022-11-26 17:01:25 +0600

    JWT API in server side is done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dl1tykd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// token function
+
+//jwt token function
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access')
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 async function run() {
     try {
@@ -44,11 +65,17 @@ async function run() {
         });
 
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            // console.log('token', req.headers.authorization)
+            const decodedEmail = req.decoded.email;
+
+            if (email !== decodedEmail) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
             const query = { email: email }
             const bookings = await bookingsCollection.find(query).toArray();
-            res.send(bookings)
+            res.send(bookings);
         });
 
         app.post('/bookings', async (req, res) => {
@@ -58,12 +85,25 @@ async function run() {
             res.send(result);
         });
 
+        //JWT TOKEN
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '2h' });
+                return res.send({ accessToken: token });
+            }
+            console.log(user)
+            res.status(403).send({ accessToken: '' });
+        })
+
         // save user 
         app.post('/users', async (req, res) => {
             const user = req.body;
             console.log(user)
             const result = usersCollection.insertOne(user);
-            res.send(result)
+            res.send(result);
         });
 
     }
