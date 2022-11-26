6df3603780a37b093ddd3d84e4a547a67afbe9a6
mintusarker <sarkermintu707@gmail.com>
2022-11-26 13:35:46 +0600

    my orders API section done

--- index.js
+++ index.js
@@ -42,13 +42,20 @@ async function run() {
 
         });
 
+
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email}
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings)
+        })
+
         app.post('/bookings', async(req ,res) =>{
             const booking = req.body;
             console.log(booking);
             const result = await bookingsCollection.insertOne(booking);
             res.send(result);
-        })
-
+        });
 
     }
     finally {
