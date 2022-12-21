c987bd6b11d79944530d8042bd9a3f1bdf54e01a
mintusarker <sarkermintu707@gmail.com>
2022-12-21 12:58:59 +0600

    odified

--- index.js
+++ index.js
@@ -247,26 +247,26 @@ async function run() {
         })
 
         // //make admin
-        // app.put('/users/admin/:id', verifyJWT, async (req, res) => {
-        //     const decodedEmail = req.decoded.email;
-        //     const query = { email: decodedEmail };
-        //     const user = await usersCollection.findOne(query);
+        app.put('/users/admin/:id', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const query = { email: decodedEmail };
+            const user = await usersCollection.findOne(query);
 
-        //     if (user.role !== 'admin') {
-        //         return res.status(403).send({ message: 'forbidden access' })
-        //     }
+            if (user.role !== 'admin') {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
 
-        //     const id = req.params.id;
-        //     const filter = { _id: ObjectId(id) }
-        //     const options = { upsert: true };
-        //     const updatedDoc = {
-        //         $set: {
-        //             role: 'admin'
-        //         }
-        //     }
-        //     const result = await usersCollection.updateOne(filter, updatedDoc, options);
-        //     res.send(result);
-        // })
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
         // my product
         app.get('/products', async (req, res) => {
